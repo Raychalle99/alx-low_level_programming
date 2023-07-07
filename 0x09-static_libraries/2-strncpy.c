a0634a27b433f4fde17e4c0e246d4711dfdba03f
Raychalle99 <raychallemburu@gamil.com>
2023-07-07 02:58:09 -0700

    0x09-static_libraries

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncpy - function that copies a string
+ * @dest: destination string pointer
+ * @src: source string pointer
+ * @n: number o bytes
+ * Return: pointer to destination string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+        int numbers;
+
+        for (numbers = 0; numbers < n && src[numbers] != '\0'; numbers++)
+                dest[numbers] = src[numbers];
+        {
+                for (; numbers < n; numbers++)
+                        dest[numbers] = '\0';
+        }
+        return (dest);
+}
